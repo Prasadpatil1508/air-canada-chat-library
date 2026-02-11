d748b5431811e26101c9a170fb7a602aafe950f2
Prasad Patil <prasadpatil1508@gmail.com>
2026-02-11 11:36:32 +0530

    Update ChatSDK binary target URL in Package.swift
    - Changed the binary target URL for ChatSDK to the latest asset release for improved access.
    - Ensured the checksum remains consistent with the updated asset for integrity verification.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Host app MUST have ~/.netrc with GitHub PAT for api.github.com
         .binaryTarget(
             name: "ChatSDK",
-            url: "https://api.github.com/repos/Prasadpatil1508/air-canada-chat-library/releases/assets/353629955.zip",
+            url: "https://api.github.com/repos/Prasadpatil1508/air-canada-chat-library/releases/assets/354033679.zip",
             checksum: "317ef68a3d9e46107a119e684301293310325d3be164c9518248cdbcd83c33c8"
         )
     ]
